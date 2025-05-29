3aa9ef47d4384d8beae47b88a576c58831d1c3f6
Tecnojack <jipalaciosc@unac.edu.co>
2025-05-29 01:25:55 -0500

    Se elimina componente sin uso

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
-import { HeroesComponent } from './heroes.component';
 import { HeroesListComponent } from './pages/heroes-list/heroes-list.component';
 import { HeroDetailComponent } from './pages/hero-detail/hero-detail.component';
 import { SharedModule } from '../shared/shared.module';
@@ -12,7 +11,6 @@ import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    HeroesComponent,
     HeroesListComponent,
     HeroDetailComponent
   ],
